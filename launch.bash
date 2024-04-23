9107c22fbed1839edfaf5d83cc71faa937a6613b
Noah Curran <swimnoah113@gmail.com>
2024-04-23 18:59:10 +0000

    typo

--- launch.bash
+++ launch.bash
@@ -1,5 +1,5 @@
 source /opt/ros/foxy/setup.bash
-cd /home/ntcurran/Code/f1tenth_labs/f1tenth_lab3_template/f1tenth_gym_ros/sim_ws
+cd /home/ntcurran/Code/f1tenth_labs/f1tenth_lab4_template/f1tenth_gym_ros/sim_ws
 colcon build
 source install/local_setup.bash
 ros2 launch f1tenth_gym_ros gym_bridge_launch.py
